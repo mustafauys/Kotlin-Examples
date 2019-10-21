4d62e4db0224117a7ce74365c8585ce51d3b676f
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-10-22 00:35:15 +0300

    Visual Sequence

--- CatchTheKenny/app/src/main/java/com/mustafauysal/catchthekenny/MainActivity.java
+++ CatchTheKenny/app/src/main/java/com/mustafauysal/catchthekenny/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     TextView scoreText;
     TextView timeText;
     int score;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
         timeText = (TextView) findViewById(R.id.timeText);
         scoreText = (TextView) findViewById(R.id.scoreText);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+        imageArray = new ImageView[] {imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9};
+
+        hideImages();
 
         score = 0;
 
@@ -46,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
         //score  = score + 1;
 
         scoreText.setText("Score: " + score);
+
+    }
+
+    public void hideImages() {
+
+        for (ImageView image : imageArray) {
+            image.setVisibility(View.INVISIBLE);
+        }
 
     }
 }
