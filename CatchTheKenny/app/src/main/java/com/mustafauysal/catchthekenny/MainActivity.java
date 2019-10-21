47adfbec55424664e126b960677965d80679905c
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-10-21 23:47:22 +0300

    Increase The Score

--- CatchTheKenny/app/src/main/java/com/mustafauysal/catchthekenny/MainActivity.java
+++ CatchTheKenny/app/src/main/java/com/mustafauysal/catchthekenny/MainActivity.java
@@ -2,12 +2,36 @@ package com.mustafauysal.catchthekenny;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView scoreText;
+    TextView timeText;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize
+
+        timeText = (TextView) findViewById(R.id.timeText);
+        scoreText = (TextView) findViewById(R.id.scoreText);
+
+        score = 0;
+    }
+
+    public void increaseScore (View view) {
+
+        score++;
+        //score  = score + 1;
+
+        scoreText.setText("Score: " + score);
+
     }
 }
