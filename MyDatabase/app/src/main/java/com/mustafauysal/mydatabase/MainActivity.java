a122a799c8c159e90f1f556980cd4a04c4a7261a
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-10-24 22:57:41 +0300

    Data Withdrawal

--- MyDatabase/app/src/main/java/com/mustafauysal/mydatabase/MainActivity.java
+++ MyDatabase/app/src/main/java/com/mustafauysal/mydatabase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mustafauysal.mydatabase;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +17,24 @@ public class MainActivity extends AppCompatActivity {
 
             myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT(2))");
 
-            myDatabase.execSQL("INSERT INTO musicians (name, age) VALUES ('James', 50)");
+            //myDatabase.execSQL("INSERT INTO musicians (name, age) VALUES ('James', 50)");
+
+            //myDatabase.execSQL("INSERT INTO musicians (name, age) VALUES ('Lars', 55)");
+
+            Cursor cursor = myDatabase.rawQuery("SELECT * FROM musicians", null);
+
+            int nameIx = cursor.getColumnIndex("name");
+            int ageIx = cursor.getColumnIndex("age");
+
+            cursor.moveToFirst();
+
+            while (cursor != null) {
+                System.out.println("Name: " + cursor.getString(nameIx));
+                System.out.println("Age: " + cursor.getInt(ageIx));
+                cursor.moveToNext();
+            }
+
+
         }   catch (Exception e) {
             e.printStackTrace();
         }
