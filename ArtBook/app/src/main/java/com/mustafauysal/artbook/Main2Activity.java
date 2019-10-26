c22a29f3a88967aa2b2d7cfc5312b65074f4863a
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-10-26 22:36:46 +0300

    Second Activity Design

--- ArtBook/app/src/main/java/com/mustafauysal/artbook/Main2Activity.java
+++ ArtBook/app/src/main/java/com/mustafauysal/artbook/Main2Activity.java
@@ -2,6 +2,7 @@ package com.mustafauysal.artbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void select (View view) {
+
+    }
+
+    public void save (View view) {
+        
     }
 }
