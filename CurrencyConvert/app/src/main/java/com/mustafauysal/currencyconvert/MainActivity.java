9ff3e7ce28eac476b240a493ed6d79135a4e6fbf
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-11-03 23:18:16 +0300

    Async Task

--- CurrencyConvert/app/src/main/java/com/mustafauysal/currencyconvert/MainActivity.java
+++ CurrencyConvert/app/src/main/java/com/mustafauysal/currencyconvert/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mustafauysal.currencyconvert;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void getRates (View view) {
+
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
 }
