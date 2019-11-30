30aa9bc21ce9d4618804a82be26812c73059b524
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-11-30 23:42:20 +0300

    Calculator Codes

--- KotlinSimpleCalculator/app/src/main/java/com/musta/kotlinsimplecalculator/MainActivity.kt
+++ KotlinSimpleCalculator/app/src/main/java/com/musta/kotlinsimplecalculator/MainActivity.kt
@@ -2,11 +2,53 @@ package com.musta.kotlinsimplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sum (view: View) {
+
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+
+        val result = a + b
+
+        textView.text = "Result: $result"
+    }
+
+    fun sub (view: View) {
+
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+
+        val result = a - b
+
+        textView.text = "Result: $result"
+    }
+
+    fun multiply (view: View) {
+
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+
+        val result = a * b
+
+        textView.text = "Result: $result"
+    }
+
+    fun div (view: View) {
+
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+
+        val result = a / b
+
+        textView.text = "Result: $result"
     }
 }
