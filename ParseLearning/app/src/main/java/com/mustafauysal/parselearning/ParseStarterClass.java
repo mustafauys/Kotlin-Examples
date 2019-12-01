cb2d235152d9603ab14154fc511df1029e5620f2
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-02 02:50:32 +0300

    App Completed

---
+++ ParseLearning/app/src/main/java/com/mustafauysal/parselearning/ParseStarterClass.java
@@ -0,0 +1,16 @@
+package com.mustafauysal.parselearning;
+
+import android.app.Application;
+
+
+
+public class ParseStarterClass extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+
+    }
+}
