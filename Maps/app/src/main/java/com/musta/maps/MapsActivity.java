9de2fecdc93641058d114ec5e2c6a6a77731bd1c
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-06 04:00:30 +0300

    Users Seeing Address

--- Maps/app/src/main/java/com/musta/maps/MapsActivity.java
+++ Maps/app/src/main/java/com/musta/maps/MapsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -15,6 +16,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,7 +29,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
 
@@ -57,19 +59,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15)); */
-
-                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-
-                try {
-                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-
-                    if (addressList != null && addressList.size() > 0) {
-                        System.out.println("Address: " + addressList.get(0).toString());
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
             }
 
@@ -120,7 +109,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLastLocation, 15));
         }
 
-
+        mMap.setOnMapLongClickListener(this);
 
 
     }
@@ -137,5 +126,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        mMap.clear();
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        String address = "";
+
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if (addressList != null && addressList.size() > 0) {
+                if (addressList.get(0).getThoroughfare() != null) {
+                    address += addressList.get(0).getThoroughfare();
+
+                    if (addressList.get(0).getSubThoroughfare() != null) {
+                        address += addressList.get(0).getSubThoroughfare();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (address.matches("")) {
+            address = "No Address";
+        }
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
     }
 }
