148b2a4564703f30902a66a70ca906f4212c17f1
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-07 19:16:01 +0300

    New Travel Book

---
+++ NewTravelBook/app/src/main/java/com/musta/newtravelbook/MainActivity.java
@@ -0,0 +1,42 @@
+package com.musta.newtravelbook;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.add_place, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_place) {
+            
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+    }
+
+
+}
