2be6e47218b10c8984f3f106926a11a117da5591
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-08 03:12:19 +0300

    Adding Maps

--- NewTravelBook/app/src/main/java/com/musta/newtravelbook/MainActivity.java
+++ NewTravelBook/app/src/main/java/com/musta/newtravelbook/MainActivity.java
@@ -3,6 +3,7 @@ package com.musta.newtravelbook;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,19 +14,26 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Object MenuInflater;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater = getMenuInflater();
         getMenuInflater().inflate(R.menu.add_place, menu);
-        return super.onCreateOptionsMenu(menu);
+
+
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         if (item.getItemId() == R.id.add_place) {
-            
+            Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(intent);
+
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
