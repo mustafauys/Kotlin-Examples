121b23d3f576127ba66196926aabce994a4033cc
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-13 23:44:12 +0300

    Extracting Data from the Database

--- NewTravelBook/app/src/main/java/com/musta/newtravelbook/MainActivity.java
+++ NewTravelBook/app/src/main/java/com/musta/newtravelbook/MainActivity.java
@@ -4,26 +4,33 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Object MenuInflater;
+    static ArrayList<String> names = new ArrayList<String>();
+    static ArrayList<LatLng> locations = new ArrayList<LatLng>();
+    static ArrayAdapter arrayAdapter;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuInflater = getMenuInflater();
-        getMenuInflater().inflate(R.menu.add_place, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_place, menu);
 
+        return super.onCreateOptionsMenu(menu);
 
-        return true;
     }
 
     @Override
@@ -33,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
             startActivity(intent);
 
-            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -44,6 +50,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView = (ListView) findViewById(R.id.listView);
+
+        try {
+
+            MapsActivity.database = this.openOrCreateDatabase("Places", MODE_PRIVATE, null);
+            Cursor cursor = MapsActivity.database.rawQuery("SELECT * FROM places", null);
+
+            int nameIx = cursor.getColumnIndex("name");
+            int latitudeIx = cursor.getColumnIndex("latitude");
+            int longitudeIx = cursor.getColumnIndex("longitude");
+
+
+            while (cursor.moveToNext()) {
+
+                String nameFromDatabase = cursor.getString(nameIx);
+                String latitudeFromDatabase = cursor.getString(latitudeIx);
+                String longitudeFromDatabase = cursor.getString(longitudeIx);
+
+                names.add(nameFromDatabase);
+
+                Double l1 = Double.parseDouble(latitudeFromDatabase);
+                Double l2 = Double.parseDouble(longitudeFromDatabase);
+
+                LatLng locationFromDatabase = new LatLng(l1, l2);
+
+                locations.add(locationFromDatabase);
+                
+            }
+
+            cursor.close();
+
+        } catch (Exception e) {
+
+        }
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,names);
+
+        listView.setAdapter(arrayAdapter);
     }
 
 
