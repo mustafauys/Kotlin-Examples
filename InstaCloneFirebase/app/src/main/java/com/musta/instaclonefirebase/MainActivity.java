c2bba6d17804966a55f5db9d1f0603521d138355
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-16 02:53:22 +0300

    User Registration

--- InstaCloneFirebase/app/src/main/java/com/musta/instaclonefirebase/MainActivity.java
+++ InstaCloneFirebase/app/src/main/java/com/musta/instaclonefirebase/MainActivity.java
@@ -1,16 +1,54 @@
 package com.musta.instaclonefirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    EditText emailText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+
+    }
+
+    public void signInClicked (View view) {
+
+    }
+
+    public void signUpClicked (View view) {
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this,"User Created", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
