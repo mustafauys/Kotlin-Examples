1e6322f4a943949e42f664626bac905054157825
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-17 02:18:54 +0300

    User Interface

--- InstaCloneFirebase/app/src/main/java/com/musta/instaclonefirebase/UploadActivity.java
+++ InstaCloneFirebase/app/src/main/java/com/musta/instaclonefirebase/UploadActivity.java
@@ -3,6 +3,7 @@ package com.musta.instaclonefirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+    }
+
+    public void upload (View view) {
+
+    }
+
+    public void selectImage (View view) {
+        
     }
 }
