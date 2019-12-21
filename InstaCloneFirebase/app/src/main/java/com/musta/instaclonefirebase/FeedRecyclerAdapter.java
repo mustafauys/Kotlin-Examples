139911342650481b6307574bb991281677eac6ec
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-21 04:00:08 +0300

    Recycler Adapter

---
+++ InstaCloneFirebase/app/src/main/java/com/musta/instaclonefirebase/FeedRecyclerAdapter.java
@@ -0,0 +1,51 @@
+package com.musta.instaclonefirebase;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FeedRecyclerAdapter extends RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder> {
+
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.recycler_row,parent,false);
+        return new PostHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+
+        holder.userEmailText.setText("");
+        holder.commentText.setText("");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class PostHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView userEmailText;
+        TextView commentText;
+
+        public PostHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.recyclerview_row_imageview);
+            userEmailText = itemView.findViewById(R.id.recyclerview_row_useremail_text);
+            commentText = itemView.findViewById(R.id.recyclerview_row_comment_text);
+        }
+    }
+}
