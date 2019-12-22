d7461c3083a02453137fda8caac34b5bb37012e0
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-22 22:52:50 +0300

    Parse Settings

--- ParseLearning/app/src/main/java/com/mustafauysal/parselearning/ParseStarterClass.java
+++ ParseLearning/app/src/main/java/com/mustafauysal/parselearning/ParseStarterClass.java
@@ -2,7 +2,7 @@ package com.mustafauysal.parselearning;
 
 import android.app.Application;
 
-
+import com.parse.Parse;
 
 public class ParseStarterClass extends Application {
 
@@ -10,7 +10,14 @@ public class ParseStarterClass extends Application {
     public void onCreate() {
         super.onCreate();
 
-
+        //Set Log Level
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("")
+                .clientKey("")
+                .server("")
+                .build()
+        );
 
     }
 }
