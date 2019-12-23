5d223fb1b21eedd234359a740f833d2aa90682c3
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-23 23:16:41 +0300

    Saving Data

--- ParseLearning/app/src/main/java/com/mustafauysal/parselearning/MainActivity.java
+++ ParseLearning/app/src/main/java/com/mustafauysal/parselearning/MainActivity.java
@@ -2,12 +2,33 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+
+    public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseObject object = new ParseObject("Fruits");
+
+        object.put("name", "orange");
+        object.put("calories", 150);
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Object Saved",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
