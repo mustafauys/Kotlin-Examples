daede81cf46980d5e9f768dc95f175f5d361a65f
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-26 23:57:18 +0300

    Sign Up Design

--- InstagramCloneParse/app/src/main/java/com/musta/instagramcloneparse/SignUpActivity.java
+++ InstagramCloneParse/app/src/main/java/com/musta/instagramcloneparse/SignUpActivity.java
@@ -3,14 +3,29 @@ package com.musta.instagramcloneparse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText usernameText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        usernameText = findViewById(R.id.sign_up_activity_name_text);
+        passwordText = findViewById(R.id.sign_up_activity_password_text);
 
+
+    }
+
+    public void signIn (View view) {
+
+    }
+
+    public void signUp (View view) {
+        
     }
 }
