2e04f796979b6223c25b002cbb69750964981139
UysalMustafaa <uysall.mustafaa@gmail.com>
2019-12-27 21:51:45 +0300

    Recording

--- InstagramCloneParse/app/src/main/java/com/musta/instagramcloneparse/SignUpActivity.java
+++ InstagramCloneParse/app/src/main/java/com/musta/instagramcloneparse/SignUpActivity.java
@@ -2,9 +2,16 @@ package com.musta.instagramcloneparse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -22,10 +29,38 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void signIn (View view) {
+        ParseUser.logInInBackground(usernameText.getText().toString(), passwordText.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Welcome " + user.getUsername(),Toast.LENGTH_LONG).show();
 
+                    Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     public void signUp (View view) {
-        
+        ParseUser user = new ParseUser();
+        user.setUsername(usernameText.getText().toString());
+        user.setPassword(passwordText.getText().toString());
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"User Created!",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
     }
 }
