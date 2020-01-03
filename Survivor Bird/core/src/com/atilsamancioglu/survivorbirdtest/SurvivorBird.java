44041240b8b5e05c62f2e166a73e74908f844f63
UysalMustafaa <uysall.mustafaa@gmail.com>
2020-01-03 22:49:36 +0300

    Drawing the Bird

--- Survivor Bird/core/src/com/atilsamancioglu/survivorbirdtest/SurvivorBird.java
+++ Survivor Bird/core/src/com/atilsamancioglu/survivorbirdtest/SurvivorBird.java
@@ -10,18 +10,25 @@ public class SurvivorBird extends ApplicationAdapter {
 
 	SpriteBatch batch;
 	Texture background;
+	Texture bird;
+	float birdX = 0;
+	float birdY = 0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("background.png");
+		bird = new Texture("bird.png");
+
+		birdX = Gdx.graphics.getWidth() / 2 - bird.getHeight() / 1;
+		birdY = Gdx.graphics.getHeight() / 3;
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-
+		batch.draw(bird,birdX,birdY, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
 
 		batch.end();
 	}
