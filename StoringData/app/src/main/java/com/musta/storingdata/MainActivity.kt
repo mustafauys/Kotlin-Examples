b36470ab553c120fe13a1fa9ce03c34034b24a1f
UysalMustafaa <uysall.mustafaa@gmail.com>
2020-01-08 22:11:19 +0300

    Data Retention

--- StoringData/app/src/main/java/com/musta/storingdata/MainActivity.kt
+++ StoringData/app/src/main/java/com/musta/storingdata/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.musta.storingdata
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun save (view : View) {
+
+        //SharedPreferences
+
+        val sharedPreferences = this.getSharedPreferences("com.musta.storingdata", Context.MODE_PRIVATE)
+
+        val myAge = editText.text.toString().toIntOrNull()
+
+        if (myAge != null) {
+            textView.text = "Your Age: " + myAge
+            sharedPreferences.edit().putInt("age", myAge)
+        }
+
+    }
+
+    fun delete (view : View) {
+
     }
 }
