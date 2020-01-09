e95243bc223712a6149767811e87e901e70e6094
UysalMustafaa <uysall.mustafaa@gmail.com>
2020-01-09 20:38:02 +0300

    Reading Data

--- StoringData/app/src/main/java/com/musta/storingdata/MainActivity.kt
+++ StoringData/app/src/main/java/com/musta/storingdata/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.musta.storingdata
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,22 +9,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var sharedPreferences : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //SharedPreferences Initialize
+        sharedPreferences = this.getSharedPreferences("com.musta.storingdata", Context.MODE_PRIVATE)
+
+        val ageFromPreferences = sharedPreferences.getInt("age", -1)
+
+        if (ageFromPreferences == -1) {
+            textView.text = "Your Age: "
+        } else {
+            textView.text = "Your Age:  $ageFromPreferences"
+        }
     }
 
     fun save (view : View) {
 
         //SharedPreferences
 
-        val sharedPreferences = this.getSharedPreferences("com.musta.storingdata", Context.MODE_PRIVATE)
 
         val myAge = editText.text.toString().toIntOrNull()
 
         if (myAge != null) {
             textView.text = "Your Age: " + myAge
-            sharedPreferences.edit().putInt("age", myAge)
+            sharedPreferences.edit().putInt("age", myAge).apply()
         }
 
     }
