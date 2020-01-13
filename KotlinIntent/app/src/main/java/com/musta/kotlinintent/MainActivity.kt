1b61a99186f868e1e8d6d121951ff04b88b11b94
UysalMustafaa <uysall.mustafaa@gmail.com>
2020-01-13 23:13:11 +0300

    Active Life Cycle

--- KotlinIntent/app/src/main/java/com/musta/kotlinintent/MainActivity.kt
+++ KotlinIntent/app/src/main/java/com/musta/kotlinintent/MainActivity.kt
@@ -11,6 +11,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("on create called")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        println("on resume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("on pause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("on stop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("on destroy called")
     }
 
     fun next (view: View) {
@@ -19,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("username", userNameText.text.toString())
         intent.putExtra("name",nameText.text.toString())
         startActivity(intent)
+        finish()
 
     }
 }
