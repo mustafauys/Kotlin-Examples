9625c373e986705d6dd13c9f04f55b5dafcd3442
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-05-15 23:52:18 +0300

    Simple Stopwatch

--- KotlinRunnables/app/src/main/java/com/musta/kotlinrunnables/MainActivity.kt
+++ KotlinRunnables/app/src/main/java/com/musta/kotlinrunnables/MainActivity.kt
@@ -8,37 +8,5 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var number = 0
-    var runnable : Runnable = Runnable { }
-    var handler : Handler = Handler()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun start(view: View) {
-
-        number = 0
-
-        runnable = object : Runnable {
-            override fun run() {
-                number = number + 1
-                textView.text = "Time : $number"
-
-                handler.postDelayed(this, 1000)
-            }
-
-        }
-        handler.post(runnable)
-
-    }
-
-    fun stop (view: View) {
-
-        handler.removeCallbacks(runnable)
-        number = 0
-        textView.text = "Time: 0"
-
-    }
+   
 }
