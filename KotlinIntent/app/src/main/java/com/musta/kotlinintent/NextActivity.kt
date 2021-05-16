cc0b3576736cdb5281878162f4d55a678626c227
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-05-16 23:03:44 +0300

    Data Transfer

--- KotlinIntent/app/src/main/java/com/musta/kotlinintent/NextActivity.kt
+++ KotlinIntent/app/src/main/java/com/musta/kotlinintent/NextActivity.kt
@@ -7,17 +7,6 @@ import kotlinx.android.synthetic.main.activity_next.*
 
 class NextActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_next)
-
-        //getIntent
-
-        val intent = intent
-        val username = intent.getStringExtra("username")
-        val name = intent.getStringExtra("name")
-
-        userNameTextNextActivity.text = "Username: " + username
-        NameTextNextActivity.text = "Name: " + name
+  
     }
 }
